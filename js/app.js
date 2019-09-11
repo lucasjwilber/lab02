1b86fe393fc93e08c98adf78a76127c7713ccfb4
Lucas Wilber <lucasjwilber@gmail.com>
2019-09-11 14:13:47 -0700

    redid most of the JS to use arrays and loops

--- js/app.js
+++ js/app.js
@@ -1,59 +1,59 @@
 'use strict'
-var visitorName = prompt("Welcome to my site! Who is this?");
-document.getElementById("welcomeVisitorMessage").textContent = `Greetings, ${visitorName}!`;
+//0 = questions
+//1 = answers
+//2 = responses
+var randomNumber = Math.round(Math.random() * 10);
 var quizScore = 0;
-//start quiz. first question:
-var firstQuestion = prompt(`Welcome to my site ${visitorName}. Ready for a short quiz?`).toLowerCase();
-console.log(`first question answered with "${firstQuestion}"`);
-if (firstQuestion === "yes" || firstQuestion === "y") {
-    console.log("question answered correctly");
-    var secondQuestion = prompt(` Great! One point for you! Only four questions left. \n Question 2: Would the following code give an error? \n alert('I'm eating an apple');`).toLowerCase();
-    quizScore++;
-} else {
-    console.log("question answered incorrectly");
-    var secondQuestion = prompt(`Too bad, you've already started. And now you're 0 for 1. Only four questions left. \n Question 2: Would the following code give an error? \n alert('I'm eating an apple');`).toLowerCase();
+var guessingGame = [
+    //index 0 = questions
+    [`Does an 8oz can of Redbull have more caffeine than an 8oz cup of coffee?`,
+        `Are there 3 different characters you can use to encapsulate strings?`,
+        `Would the following code give an error? \n alert('I'm eating an apple');`,
+        `Yes or no: I was in the army.`,
+        `Do I own a cat?`,
+        `Guess a number between 1 and 10. You have 4 chances!`,
+        `What is my favorite color?`,
+    ],
+    //index 1 = correct answers shorthand
+    [`n`,
+        `y`,
+        `y`,
+        `n`,
+        `n`,
+        randomNumber,
+        `grey`,
+    ],
+    //index 2 = correct answers full
+    [`no`,
+        `yes`,
+        `yes`,
+        `no`,
+        `no`,
+    ],
+    //index 3 = user responses
+    [`placeholder`,
+        `placeholder`,
+        `placeholder`,
+        `placeholder`,
+        `placeholder`,
+        `placeholder`,
+        `placeholder`,
+    ],
+];
+
+var visitorName = prompt("Welcome to my site! Who is this?");
+alert(`Welcome to my site ${visitorName}. Let's play a guessing game.`);
+
+//questions loop
+for (var i = 0; i < 5; i++) {
+    guessingGame[3][i] = prompt(guessingGame[0][i]);
+    guessingGame[3][i] = guessingGame[3][i].toLowerCase();
+    console.log(guessingGame[3][i]);
+    if (guessingGame[3][i] === guessingGame[1][i]
+        || guessingGame[3][i] === guessingGame[2][i]) {
+            alert(`Correct!`);
+            quizScore++;
+    } else {
+        alert(`Incorrect :(`);
+    }
 }
-console.log(`second question answered with "${secondQuestion}"`);
-//second question:
-if (secondQuestion === "yes" || secondQuestion === "y") {
-    console.log("question answered correctly");
-    var thirdQuestion = prompt(`Correct! \n Third question: Does an 8oz can of Redbull have more caffeine than an 8oz cup of coffee?`).toLowerCase();
-    quizScore++;
-} else {
-    console.log("question answered incorrectly");
-    var thirdQuestion = prompt(`Wrong! \n Third question: Does an 8oz can of Redbull have more caffeine than an 8oz cup of coffee?`).toLowerCase();
-}
-console.log(`third question answered with "${thirdQuestion}"`);
-//third question
-if (thirdQuestion === "yes" || thirdQuestion === "y") {
-    console.log("question answered incorrectly");
-    var fourthQuestion = prompt(`False! \n Fourth question: Are there 3 different characters you can use to encapsulate strings?`).toLowerCase();
-} else {
-    console.log("question answered correctly");
-    var fourthQuestion = prompt(`That's right! \n Fourth question: Are there 3 different characters you can use to encapsulate strings?`).toLowerCase();
-    quizScore++;
-}
-console.log(`fourth question answered with "${fourthQuestion}"`);
-//fourth question
-if (fourthQuestion === "yes" || fourthQuestion === "y") {
-    console.log("question answered correctly");
-    var fifthQuestion = prompt(`Correct! \n Last question: Is Au the chemical symbol for Australium?`).toLowerCase();
-    quizScore++;
-} else {
-    console.log("question answered incorrectly");
-    var fifthQuestion = prompt(`Incorrect! The ', ", and \` characters all work with strings. \n Last question: Is Au the chemical symbol for Australium?`).toLowerCase();
-}
-console.log(`fifth question answered with "${fifthQuestion}"`);
-//fifth question
-if (fifthQuestion === "yes" || fifthQuestion === "y") {
-    console.log("question answered correctly");
-    alert(`Nope! \n Test complete, you scored ${quizScore} out of 5`);
-} else {
-    console.log("question answered correctly");
-    quizScore++;
-    alert(`Good job. \n You scored ${quizScore} out of 5`);
-}
-console.log(`total score for ${visitorName}: ${quizScore}`);
-console.log(`"LiVe ReLoAd EnAbLeD."`);
-//footer text
-document.getElementById("footer").textContent = `Thanks for visiting, ${visitorName}.`;
