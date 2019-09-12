4098928084eb7f4717187ea7aaf1790e56946b8a
Lucas Wilber <lucasjwilber@gmail.com>
2019-09-12 10:37:09 -0700

    added another loop to the end to display possible answers for q7. changed whitespacing again

--- js/app.js
+++ js/app.js
@@ -34,7 +34,6 @@ var guessingGame = [
 ];
 
 var visitorName = prompt('Welcome to my site! Who is this?');
-
 alert(`Welcome to my site ${visitorName}. Let's play a guessing game.`);
 
 //questions loop
@@ -49,16 +48,16 @@ for (var i = 0; i < 5; i++) {
   }
 
   //check for correct answer
-  if ((guessingGame[3][i] === guessingGame[1][i])
+  if (
+    (guessingGame[3][i] === guessingGame[1][i])
     || (guessingGame[3][i] === guessingGame[2][i])
-    && (i !== 5)) {
-
+    && (i !== 5)
+  ) {
     alert('Correct!');
     quizScore++;
     console.log(`Question; ${guessingGame[0][i]}. Correct answer: ${guessingGame[2][i]} or ${guessingGame[1][i]}. User answered with ${guessingGame[3][i]}`);
 
   } else {
-
     alert('Incorrect :(');
   }
 }
@@ -66,19 +65,16 @@ for (var i = 0; i < 5; i++) {
 //question 6
 var randomNumber = (Math.ceil(Math.random() * 10)).toString();
 console.log(`number is ${randomNumber}`);
-
 var attempts = 3;
 var numberGuess = prompt('Guess a number between 1 and 10. You have 4 tries left!');
 
 while ((numberGuess !== randomNumber) && (attempts > 0)) {
-
   if (numberGuess > randomNumber) {
     numberGuess = prompt(`Too high! You have ${attempts} tries left!`);
     attempts--;
     console.log(`user guessed ${numberGuess}, but the number is ${randomNumber}`);
-
-  } else {
-
+  }
+  else {
     numberGuess = prompt(`Too low! You have ${attempts} tries left!`);
     attempts--;
     console.log(`user guessed ${numberGuess}, but the number is ${randomNumber}`);
@@ -86,14 +82,13 @@ while ((numberGuess !== randomNumber) && (attempts > 0)) {
 }
 
 if (numberGuess === randomNumber) {
-
   alert(`Correct! The number was ${randomNumber}`);
   quizScore++;
-
-} else {
-
+}
+else {
   alert(`Out of tries! The number was ${randomNumber}`);
 }
+
 
 //question 7
 var correctAnswers = ['moira', 'ana', 'mercy', 'baptiste', 'zenyatta', 'lucio', 'brigitte',];
@@ -107,26 +102,27 @@ do {
   guesses--;
 
   for (i = 0; i < correctAnswers.length; i++) {
-
     if (answerGuess === correctAnswers[i]) {
       gotItRight = true;
       console.log(`you guessed ${answerGuess}, you have ${guesses} guesses left`);
     }
-
   }
-} while ((gotItRight === false) && (guesses > 0));
+} while (
+  (gotItRight === false) && (guesses > 0)
+);
+
 
 
 if (gotItRight === true) {
-
   alert(`Well done ${visitorName}! That's correct!`);
   quizScore++;
-
-} else {
-
-  alert(`Sorry ${visitorName}, you're out of tries. \n The possible answers were ${correctAnswers[0]}, ${correctAnswers[1]},
-    ${correctAnswers[2]}, ${correctAnswers[3]}, ${correctAnswers[4]}, ${correctAnswers[5]}, or ${correctAnswers[6]}.`);
-
+}
+else {
+  var answerString = '';
+  for (let i = 0; i < correctAnswers.length; i++) {
+    answerString += (correctAnswers[i] + ', ');
+  }
+  alert(`Sorry ${visitorName}, you're out of tries. \n The possible answers were ${answerString}`);
 }
 
 alert(`Thanks for taking my quiz! Your final score is ${quizScore}/7.`);
